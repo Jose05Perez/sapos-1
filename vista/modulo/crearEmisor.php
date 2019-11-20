bf0981f4463d317c6d704b89aa26c4184ce5e636
Felix Reyes Duarte <fduarte@bn.root>
2019-11-20 11:46:03 -0400

    Maquetacion-CrearEmisor.

--- vista/modulo/crearEmisor.php
+++ vista/modulo/crearEmisor.php
@@ -10,90 +10,136 @@
 
 </section>
 <section class="content">
-
-<label>Nombre:</label>
-<div class="input-group col-md-4">
+<!-- SELECT2 EXAMPLE -->
+<div class="box box-default">
+        <div class="box-header with-border">
+        </div>
+        <!-- /.box-header -->
+        <div class="box-body">
+          <div class="row">
+            <div class="col-md-6">
+              <div class="form-group">
+                <label>Nombre:</label>
+              <div class="input-group">
                 <span class="input-group-addon"><i class="fa fa-user"></i></span>
-                <input type="email" class="form-control" placeholder="Nombre...">
-                <br>
-              </div>
-              <label>Apellido:</label>
-              <div class="input-group col-md-4">
-                <span class="input-group-addon"><i class="fa fa-users"></i></span>
-                <input type="email" class="form-control" placeholder="Apellido...">
-                <br>
-              </div>
-              <div class="form-group" >
-                <label>Fecha de Nacimiento:</label>
-
-                <div class="input-group date">
-                  <div class="input-group-addon">
-                    <i class="fa fa-calendar"></i>
-                  </div>
-                  <input type="text" class="form-control pull-right" id="datepicker">
-                </div>
-                <label>Sexo:</label>
-                <div class="input-group col-md-4">
-                <span class="input-group-addon"><i class="fa fa-venus-mars"></i></span>
-                <div class="radio radio-inline">
-                    <label>
-                      <input type="radio" name="optionsRadios" id="optionsRadios1" value="option1" checked>
-                    Hombre
-                    </label>
-                  </div>
-                  <div class="radio">
-                    <label>
-                      <input type="radio" name="optionsRadios" id="optionsRadios2" value="option2">
-                      Mujer
-                    </label>
-                  </div>
-                <br>
-              </div>
-              <label>Direccion:</label>
-              <div class="input-group col-md-4">
-                <span class="input-group-addon"><i class="fa fa-map-marker"></i></span>
-                <input type="email" class="form-control" placeholder="Direccion...">
-                <br>
-              </div>
-              <label>Cedula:</label>
-              <div class="input-group col-md-4">
-                <span class="input-group-addon"><i class="fa fa-id-card"></i></span>
-                <input type="email" class="form-control" placeholder="0-0000000-0">
-                <br>
-              </div>
-              <label>Telefono:</label>
-              <div class="input-group col-md-4">
-                <span class="input-group-addon"><i class="fa fa-phone"></i></span>
-                <input type="email" class="form-control" placeholder="(000)-000-000">
-                <br>
-              </div>
-              <label>Celular:</label>
-              <div class="input-group col-md-4">
-                <span class="input-group-addon"><i class="fa fa-phone"></i></span>
-                <input type="email" class="form-control" placeholder="(000)-000-000">
-                <br>
-              </div>
-              <label>Institucion:</label>
-              <div class="input-group col-md-4">
-                <span class="input-group-addon"><i class="fa fa-university"></i></span>
-                <select class="form-control">
-                    <option>Seleccionar Institucion</option>
-                    <option>option 2</option>
-                    <option>option 3</option>
-                    <option>option 4</option>
-                    <option>option 5</option>
-                  </select>
-                <br>
-              </div>
-              <label>E-mail:</label>
-              <div class="input-group col-md-4">
-                <span class="input-group-addon"><i class="fa fa-envelope"></i></span>
-                <input type="email" class="form-control" placeholder="E-mail...">
-                <br>
+                <input type="email" class="form-control" placeholder="Nombre">
               </div>
               <br>
-              <button type="submit" class="btn btn-primary">Guardar!!</button>
+              <!-- Sub Columna -->
+              <div class="row">
+                <div class="col-md-6">
+                  <label>Fecha de Nacimiento:</label>
+                  <div class="input-group">
+                  <span class="input-group-addon"><i class="fa fa-calendar"></i></span>
+                    <input type="text" class="form-control">
+                </div>
+              <br>
 
+                  </div>
+                  <div class="col-md-6">
+                  <label>Sexo:</label>
+                  <div class="input-group">
+                  <span class="input-group-addon"><i class="fa fa-venus-mars"></i></span>
+                    <select type="text" class="form-control">
+                    <option selected="selected">Seleccionar Sexo</option>
+                  <option>Hombre</option>
+                  <option>Mujer</option>
+                    </select>
+                </div>
+              <br>
 
+                  </div>
+              </div>
+              <!-- Cierre Sub Columna-->
+
+              <!-- Segunda Sub Columna-->
+              <div class="row">
+                <div class="col-md-6">
+                <label>Cedula:</label>
+                  <div class="input-group">
+                  <span class="input-group-addon"><i class="fa fa-id-card"></i></span>
+                    <input type="text" class="form-control" placeholder="000-0000000-0">
+                </div>
+              <br>
+
+                  </div>
+                  <div class="col-md-6">
+                  <label>Telefono:</label>
+                  <div class="input-group">
+                  <span class="input-group-addon"><i class="fa fa-phone"></i></span>
+                    <input type="text" class="form-control" placeholder="(000)-000-0000">
+                </div>
+              <br>
+
+                  </div>
+              </div>
+              <label>E-mail:</label>
+              <div class="input-group">
+              <span class="input-group-addon"><i class="fa fa-envelope"></i></span>
+                <input type="text" class="form-control" placeholder="E-mail">
+              </div>
+              <br>
+              </div>
+              <!-- /.form-group -->
+            </div>
+            <!-- /.col -->
+            <!-- Segunda Columna -->
+            <div class="col-md-6">
+            <label>Apellido:</label>
+            <div class="input-group">
+                <span class="input-group-addon"><i class="fa fa-users"></i></span>
+                <input type="email" class="form-control" placeholder="Apellido">
+              </div>
+              <br>
+              <label>Direccion:</label>
+              <div class="input-group">
+              <span class="input-group-addon"><i class="fa fa-map-marker"></i></span>
+                <input type="text" class="form-control" placeholder="Direccion">
+              </div>
+              <br>
+              <!--Sub Columna-->
+              <div class="row">
+                <div class="col-md-6">
+                <label>Celular:</label>
+                  <div class="input-group">
+                  <span class="input-group-addon"><i class="fa fa-phone"></i></span>
+                    <input type="text" class="form-control" placeholder="(000)-000-0000">
+                </div>
+              <br>
+              
+                  </div>
+                  <div class="col-md-6">
+                  <label>Institucion:</label>
+                  <div class="input-group">
+                  <span class="input-group-addon"><i class="fa fa-university"></i></span>
+                    <select type="text" class="form-control">
+                    <option selected="selected"> Seleccionar Institucion</option>
+                  <option>Ejemplo1</option>
+                    </select>
+                </div>
+              <br>
+              <br>
+              
+              
+
+                  </div>
+                  <div class="row">
+                    <div class="col-md-9">
+
+                    </div>
+                  <div class="box-footer col-md-3">
+                <button type="submit" class="btn btn-primary btn-lg">Guardar</button>
+              </div>
+                  </div>
+              </div>
+            <!-- /.col -->
+          </div>
+          
+          <!-- /.row -->
+        </div>
+        
+        <!-- /.box-body -->
+      </div>
+      <!-- /.box -->
 </section>
 </div>

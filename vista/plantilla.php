5fa5ed94fd341d171f4bd2c54ec4da69e2583d89
Felix Reyes Duarte <fduarte@bn.root>
2019-11-28 12:05:00 -0400

    Avance

--- vista/plantilla.php
+++ vista/plantilla.php
@@ -97,8 +97,8 @@ session_start();
 
 <body class="hold-transition skin-blue sidebar-mini"> 
     <?php
-        if(isset($_SESSION["iniciarSession"]) && $_SESSION["iniciarSession"] == "ok")
-        {
+        // if(isset($_SESSION["iniciarSession"]) && $_SESSION["iniciarSession"] == "ok")
+        // {
             echo '<div class="wrapper">';
                 /*===============================================================================================================
                 Incluyendo el cabezote en la aplicacion
@@ -135,7 +135,7 @@ session_start();
                 ===============================================================================================================*/
                 include "modulo/piePagina.php";  
             echo '</div>';
-        // }
+        //  }
         // else
         // {
         //     ctrUsuario::ctrIngresarUsuario(true);
